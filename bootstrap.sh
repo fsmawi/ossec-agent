b65d5c1abdea75508614d63cb9a4bb5b21ad9cd1
Fayssal Smawi <fayssal.smawi@gmail.com>
2018-06-04 16:31:09 +0000

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-[[ -z "${OSSEC_STACK_STAGE}" ]] && STACK='prod' || STACK="${OSSEC_STACK_STAGE}"
+[[ -z "${OSSEC_STACK_STAGE}" ]] && OSSEC_BUCKET='pipeline-ossec-config-prod' || OSSEC_BUCKET="pipeline-ossec-config-${OSSEC_STACK_STAGE}"
 
 # Configure SSH key.
-aws s3 cp s3://pipeline-ossec-config-${STACK}/id_ossec_agent ~/.ssh/
+aws s3 cp ${OSSEC_BUCKET}/id_ossec_agent ~/.ssh/
 chmod 600 ~/.ssh/id_ossec_agent
 
 ## Get server information
-aws s3 cp s3://pipeline-ossec-config-${STACK}/ossec-server.config .
+aws s3 cp ${OSSEC_BUCKET}/ossec-server.config .
 source ossec-server.config
 
 ## Config inventory file.
@@ -17,3 +17,9 @@ sed -i "s/REMOTE_HOST_USERNAME/centos/g" inventory
 
 ## Run ansible playbook.
 ansible-playbook -vvv -i inventory playbook.yml --extra-vars "ossec_server_name=${OSSEC_SERVER_IP} ossec_server_fqdn=${OSSEC_SERVER_FQDN}"
+
+
+
+export S3_BUCKET="s3://pipeline-ossec-config-${STACK}"
+
+aws s3 cp "s3://pipeline-ossec-config-${STACK}/id_ossec_agent" ~/.ssh/
