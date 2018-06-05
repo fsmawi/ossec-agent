7f9b4e51f15e362cc4c006852fef44ec32ddc4af
Fayssal Smawi <fayssal.smawi@gmail.com>
2018-06-05 18:38:34 +0000

    config IP

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ if [[ $AWS_ACCOUNT_ID = "672327909798" ]]; then
     OSSEC_BUCKET='s3://pipeline-ossec-config-dev'
 fi
 
+HOST_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+
 # Configure SSH key.
 aws s3 cp ${OSSEC_BUCKET}/id_ossec_agent ~/.ssh/
 chmod 600 ~/.ssh/id_ossec_agent
@@ -21,4 +23,4 @@ sed -i "s/REMOTE_HOST_IP/${OSSEC_SERVER_IP}/g" inventory
 sed -i "s/REMOTE_HOST_USERNAME/centos/g" inventory
 
 ## Run ansible playbook.
-ansible-playbook -vvv -i inventory playbook.yml --extra-vars "ossec_server_ip=${OSSEC_SERVER_IP} ossec_server_fqdn=${OSSEC_SERVER_FQDN}"
+ansible-playbook -vvv -i inventory playbook.yml --extra-vars "ossec_server_ip=${OSSEC_SERVER_IP} ossec_server_fqdn=${OSSEC_SERVER_FQDN} ossec_agent_public_ip=${HOST_IP}"
